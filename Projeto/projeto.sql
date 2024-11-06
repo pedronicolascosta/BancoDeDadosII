81400c96c1a7b2bc8867db48ec9c772105522dd2
Pedro Nicolas Costa <pedroniickolas@gmail.com>
2024-11-06 15:15:42 -0300

    Update projeto.sql

--- Projeto/projeto.sql
+++ Projeto/projeto.sql
@@ -12,7 +12,7 @@
 CREATE DATABASE MartorelliTattoo;
 USE MartorelliTattoo;
 
--- CRIAÇÃO TABELA CLIENTE - PEDRO
+-- CRIAÇÃO TABELA CLIENTE
 CREATE TABLE Cliente(
     nome VARCHAR(60) NOT NULL,
     codigoCliente INT NOT NULL AUTO_INCREMENT,
@@ -25,7 +25,7 @@ CREATE TABLE Cliente(
     whatsapp VARCHAR(15) NOT NULL
 );
 
--- CRIAÇÃO DA TABELA ESTILO - LANZI
+-- CRIAÇÃO DA TABELA ESTILO
 CREATE TABLE Estilo (
 	codigoEstilo INT NOT NULL AUTO_INCREMENT,
 	PRIMARY KEY (codigoEstilo),
@@ -34,7 +34,7 @@ CREATE TABLE Estilo (
    	tamanho INT NOT NULL
 );
 
--- CRIAÇÃO DA TABELA CLIENTE/ESTILO - PEDRO
+-- CRIAÇÃO DA TABELA CLIENTE/ESTILO
 CREATE TABLE Cliente_Estilo (
     codigoEstilo INT NOT NULL,
     codigoCliente INT NOT NULL,
@@ -43,21 +43,21 @@ CREATE TABLE Cliente_Estilo (
     PRIMARY KEY (codigoEstilo, codigoCliente)
 );
 
--- CRIAÇÃO DA TABELA TIPOESTILO - LANZI
+-- CRIAÇÃO DA TABELA TIPOESTILO
 CREATE TABLE TipoEstilo (
             idEstilo INT NOT NULL AUTO_INCREMENT,
 	PRIMARY KEY(idEstilo),
   	nomeEstilo VARCHAR(45) NOT NULL
  );
 
--- CRIAÇÃO DA TABELA CORES - PEDRO 
+-- CRIAÇÃO DA TABELA CORES 
 CREATE TABLE Cores(
     idCor INT NOT NULL AUTO_INCREMENT ,
     nomeCor VARCHAR(20) NOT NULL,
     PRIMARY KEY (idCor)
 );
 
--- CRIAÇÃO DA TABELA ESTILO/COR - LANZI
+-- CRIAÇÃO DA TABELA ESTILO/COR
 CREATE TABLE Estilo_Cor (
 	codigoEstilo INT NOT NULL ,
     idCor INT NOT NULL,
@@ -66,7 +66,7 @@ CREATE TABLE Estilo_Cor (
     PRIMARY KEY (codigoEstilo, idCor)
 );
 
--- CRIAÇÃO DA TABELA FICHA ANAMNESE - LÉO
+-- CRIAÇÃO DA TABELA FICHA ANAMNESE
 CREATE TABLE FichaAnamnese (
     codigoAnamnese INT PRIMARY KEY AUTO_INCREMENT,
     codigoCliente INT NOT NULL,
@@ -81,13 +81,13 @@ CREATE TABLE FichaAnamnese (
     FOREIGN KEY (codigoCliente) REFERENCES Cliente(codigoCliente)
 );
 
--- CRIAÇÃO DA TABELA DOENCAS - LÉO
+-- CRIAÇÃO DA TABELA DOENCAS
 CREATE TABLE Doencas (
     codigoDoenca INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nomeDoenca VARCHAR(255) NOT NULL
 );
 
--- CRIAÇÃO DA TABELA PREFERENCIAAGENDAMENTO - SOPHIA
+-- CRIAÇÃO DA TABELA PREFERENCIAAGENDAMENTO
 CREATE TABLE preferenciaAgendamento (
     codigoAgendamento INT,
     codigoCliente INT,
@@ -96,7 +96,7 @@ CREATE TABLE preferenciaAgendamento (
     PRIMARY KEY (codigoAgendamento, codigoCliente)
 );
 
--- CRIAÇÃO DA TABELA ESCOLHE - SOPHIA
+-- CRIAÇÃO DA TABELA ESCOLHE
 CREATE TABLE Escolhe (
     codigoAgendamento INT NOT NULL PRIMARY KEY,
     codigoCliente INT NOT NULL,
@@ -126,11 +126,11 @@ VALUES
 -- Inserindo clientes e seus estilos na tabela Cliente_Estilo
 INSERT INTO Cliente_Estilo (codigoEstilo, codigoCliente)
 VALUES 
-(1, 1),  -- Giovanna Léo
-(2, 2),  -- Giovana Lanzi
-(3, 3),  -- Sophia Tiene
-(4, 4),  -- Pedro Nicolas
-(5, 5);  -- Marcos Vinicius
+(1, 1),
+(2, 2), 
+(3, 3), 
+(4, 4),  
+(5, 5);  
 
 -- Inserindo tipos de estilo na tabela TipoEstilo
 INSERT INTO TipoEstilo (nomeEstilo)
@@ -153,20 +153,20 @@ VALUES
 -- Inserindo estilos e suas cores na tabela Estilo_Cor
 INSERT INTO Estilo_Cor (codigoEstilo, idCor)
 VALUES 
-(1, 1),  -- Realismo - Preto
-(2, 5),  -- Tradicional - Azul
-(3, 2),  -- Tribal - Cinza
-(4, 1),  -- Minimalista - Preto
-(5, 4);  -- Old School - Vermelho
+(1, 1),  
+(2, 5), 
+(3, 2),  
+(4, 1),  
+(5, 4);  
 
 -- Inserindo fichas de anamnese na tabela FichaAnamnese
 INSERT INTO FichaAnamnese (codigoCliente, fumante, gravida, doencaTransmissivel, epilepsia, diabete, problemaPeleCicatrizacao, tratamentoMedico, tomaRemedio)
 VALUES 
-(1, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE, TRUE),  -- Giovanna Léo
-(2, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE), -- Giovana Lanzi
-(3, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE, FALSE, TRUE),  -- Sophia Tiene
-(4, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE), -- Pedro Nicolas
-(5, TRUE, FALSE, TRUE, FALSE, TRUE, FALSE, TRUE, TRUE);     -- Marcos Vinicius
+(1, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE, TRUE),  
+(2, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE),
+(3, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE, FALSE, TRUE),  
+(4, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE), 
+(5, TRUE, FALSE, TRUE, FALSE, TRUE, FALSE, TRUE, TRUE);     
 
 -- Inserindo doenças na tabela Doencas
 INSERT INTO Doencas (nomeDoenca)
@@ -180,20 +180,20 @@ VALUES
 -- Inserindo preferências de agendamento na tabela PreferenciaAgendamento
 INSERT INTO PreferenciaAgendamento (codigoAgendamento, codigoCliente, data, hora)
 VALUES 
-(1, 1, '2024-09-15', '14:00:00'), -- Giovanna Léo
-(2, 2, '2024-09-16', '10:00:00'), -- Giovana Lanzi
-(3, 3, '2024-09-17', '11:30:00'), -- Sophia Tiene
-(4, 4, '2024-09-18', '16:00:00'), -- Pedro Nicolas
-(5, 5, '2024-09-19', '09:00:00'); -- Marcos Vinicius
+(1, 1, '2024-09-15', '14:00:00'), 
+(2, 2, '2024-09-16', '10:00:00'), 
+(3, 3, '2024-09-17', '11:30:00'), 
+(4, 4, '2024-09-18', '16:00:00'), 
+(5, 5, '2024-09-19', '09:00:00'); 
 
 -- Inserindo escolha de agendamento na tabela Escolhe
 INSERT INTO Escolhe (codigoAgendamento, codigoCliente)
 VALUES 
-(1, 1),  -- Giovanna Léo
-(2, 2),  -- Giovana Lanzi
-(3, 3),  -- Sophia Tiene
-(4, 4),  -- Pedro Nicolas
-(5, 5);  -- Marcos Vinicius
+(1, 1),  
+(2, 2),  
+(3, 3), 
+(4, 4),  
+(5, 5); 
 
 -- 4. Uma alteração de registro em cada tabela.
 -- 
@@ -228,30 +228,30 @@ VALUES
 -- 14. Usar comentários para identificar cada item.
 --
 
--- Retorna a quantidade de inscrições de preferência de agendamento para o dia atual - PEDRO
+-- Retorna a quantidade de inscrições de preferência de agendamento para o dia atual
 SELECT COUNT(*) AS quantidade_agendamentos
 FROM preferenciaAgendamento
 WHERE data = CURDATE();
 
--- Retorna o nome das pacientes que estejam grávidas e que possuam problemas de pele - PEDRO
+-- Retorna o nome das pacientes que estejam grávidas e que possuam problemas de pele
 SELECT C.nome
 FROM Cliente C JOIN FichaAnamnese F ON C.codigoCliente = F.codigoCliente
 WHERE F.gravida = TRUE
 AND F.problemaPeleCicatrizacao = TRUE;
 
--- Retorna uma lista de todos os nomes dos clientes ordenado por ordem alfabética - PEDRO
+-- Retorna uma lista de todos os nomes dos clientes ordenado por ordem alfabética 
 SELECT nome
 FROM Cliente
 ORDER BY nome ASC;
 
--- Retorna a quantidade de tatuagens com preferência de agendamento para um dia específico que possuam mais de 10cm - PEDRO
+-- Retorna a quantidade de tatuagens com preferência de agendamento para um dia específico que possuam mais de 10cm 
 SELECT COUNT(*) AS quantidade_tatuagens
 FROM preferenciaAgendamento PA
 JOIN Cliente_Estilo CE ON PA.codigoCliente = CE.codigoCliente
 JOIN Estilo E ON CE.codigoEstilo = E.codigoEstilo
 WHERE PA.data = '2024-09-19' AND E.tamanho > 10;
 
--- Seleciona os 3 estilos de tatuagem mais frequentes - GIOVANNA
+-- Seleciona os 3 estilos de tatuagem mais frequentes
 SELECT E.tipoEstilo, COUNT(*) AS frequencia
 FROM Cliente_Estilo CE
 JOIN Estilo E ON CE.codigoEstilo = E.codigoEstilo
@@ -259,22 +259,22 @@ GROUP BY E.tipoEstilo
 ORDER BY frequencia DESC
 LIMIT 3;
 
--- Seleciona quantas pessoas têm idade maior que 40 anos - GIOVANNA
+-- Seleciona quantas pessoas têm idade maior que 40 anos 
 SELECT COUNT(*) AS quantidade
 FROM Cliente
 WHERE TIMESTAMPDIFF(YEAR, dataNascimento, CURDATE()) > 40;
 
--- Seleciona quantas pessoas têm idade maior que 20 anos - GIOVANNA
+-- Seleciona quantas pessoas têm idade maior que 20 anos 
 SELECT COUNT(*) AS quantidade
 FROM Cliente
 WHERE TIMESTAMPDIFF(YEAR, dataNascimento, CURDATE()) > 20;
 
--- Seleciona quantas pessoas fazem tratamento médico ou tomam remédio. -GIOVANNA
+-- Seleciona quantas pessoas fazem tratamento médico ou tomam remédio.
 SELECT COUNT(*) AS quantidade
 FROM FichaAnamnese
 WHERE tratamentoMedico = TRUE OR tomaRemedio = TRUE;
 
--- Retorna as 3 cores mais utilizadas - GIOVANNA
+-- Retorna as 3 cores mais utilizadas 
 SELECT C.nomeCor, COUNT(*) AS frequencia
 FROM Estilo_Cor EC
 JOIN Cores C ON EC.idCor = C.idCor
@@ -282,7 +282,7 @@ GROUP BY C.nomeCor
 ORDER BY frequencia DESC
 LIMIT 3;
 
--- Retorna os três estilos de tatuagem menos frequentes - Léo
+-- Retorna os três estilos de tatuagem menos frequentes 
 SELECT E.tipoEstilo, COUNT(*) AS frequencia
 FROM Cliente_Estilo CE
 JOIN Estilo E ON CE.codigoEstilo = E.codigoEstilo
@@ -290,7 +290,7 @@ GROUP BY E.tipoEstilo
 ORDER BY frequencia ASC
 LIMIT 3;
 
--- Retorna as cinco cores de tinta menos utilizadas nas tatuagens - Léo
+-- Retorna as cinco cores de tinta menos utilizadas nas tatuagens 
 SELECT C.nomeCor, COUNT(*) AS frequencia
 FROM Estilo_Cor EC
 JOIN Cores C ON EC.idCor = C.idCor
@@ -298,39 +298,39 @@ GROUP BY C.nomeCor
 ORDER BY frequencia ASC
 LIMIT 5;
 
--- Retorna quantas mulheres grávidas estão cadastradas - Léo
+-- Retorna quantas mulheres grávidas estão cadastradas 
 SELECT COUNT(*) AS quantidade
 FROM FichaAnamnese
 WHERE gravida = TRUE;
 
--- Retorna quantas pessoas preferem tatuar desenhos maiores que um certo tamanho (exemplo: 15 cm) - Léo
+-- Retorna quantas pessoas preferem tatuar desenhos maiores que um certo tamanho (exemplo: 15 cm) 
 SELECT COUNT(DISTINCT CE.codigoCliente) AS quantidade
 FROM Cliente_Estilo CE
 JOIN Estilo E ON CE.codigoEstilo = E.codigoEstilo
 WHERE E.tamanho > 15; 
 
--- Retorna quantas pessoas preferem tatuar desenhos menores ou iguais a um certo tamanho (exemplo: 15 cm) - Léo
+-- Retorna quantas pessoas preferem tatuar desenhos menores ou iguais a um certo tamanho (exemplo: 15 cm) 
 SELECT COUNT(DISTINCT CE.codigoCliente) AS quantidade
 FROM Cliente_Estilo CE
 JOIN Estilo E ON CE.codigoEstilo = E.codigoEstilo
 WHERE E.tamanho <= 15; -- ajuste o valor conforme necessário
 
--- Exibir todos os agendamentos com a data e o cliente associado - Sophia
+-- Exibir todos os agendamentos com a data e o cliente associado 
 SELECT PA.data, C.nome
 FROM preferenciaAgendamento PA
 JOIN Cliente C ON PA.codigoCliente = C.codigoCliente;
 
--- Listar o nome e o e-mail dos clientes cadastrados - Sophia
+-- Listar o nome e o e-mail dos clientes cadastrados 
 SELECT nome, email
 FROM Cliente;
 
--- Listar os modelos de tatuagem que os clientes solicitaram - Sophia
+-- Listar os modelos de tatuagem que os clientes solicitaram 
 SELECT C.nome, E.tipoEstilo
 FROM Cliente C
 JOIN Cliente_Estilo CE ON C.codigoCliente = CE.codigoCliente
 JOIN Estilo E ON CE.codigoEstilo = E.codigoEstilo;
 
--- Listar os clientes que informaram doenças na ficha de anamnese - Sophia
+-- Listar os clientes que informaram doenças na ficha de anamnese 
 SELECT C.nome
 FROM Cliente C
 JOIN FichaAnamnese F ON C.codigoCliente = F.codigoCliente
